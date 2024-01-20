1f3f2b03bc99ffc450060a752aeafc7a878a3a77
Andres Coronado <andres.coronado@bss.group>
2024-01-20 17:27:39 +0100

    removed cache

--- folders.cmake
+++
@@ -1,6 +0,0 @@
-# setting directories
-set_property(GLOBAL PROPERTY LIBDIR ${PROJECT_SOURCE_DIR}/includes/)
-set_property(GLOBAL PROPERTY RESDIR ${PROJECT_SOURCE_DIR}/resources)
-set_property(GLOBAL PROPERTY APPDIR ${PROJECT_SOURCE_DIR}/apps)
-set_property(GLOBAL PROPERTY KNNDIR ${PROJECT_SOURCE_DIR}/includes/algorithms)
-set_property(GLOBAL PROPERTY ROOTDIR ${PROJECT_SOURCE_DIR})
--- targets.cmake
+++
@@ -1,7 +0,0 @@
-# getting variables
-include(init.cmake)
-add_subdirectory(${LIB_DIR}/models)
-add_subdirectory(${LIB_DIR}/category-handler)
-add_subdirectory(${LIB_DIR}/data-handler)
-add_subdirectory(${KNN_DIR})
-add_subdirectory(${APPS_DIR})
